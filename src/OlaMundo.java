4927fc89d166bd4f85ac34e870c810c230abe88b
Fillipe Cordeiro <fillipe@androidpro.com.br>
2016-09-30 20:29:12 -0300

    Primeiro commit

---
+++ src/OlaMundo.java
@@ -0,0 +1,8 @@
+/**
+ * Created by fillipecordeiro on 04/07/16.
+ */
+public class OlaMundo {
+    public static void main (String[] args) {
+        System.out.println("Meu primeiro programa em java!");
+    }
+}
